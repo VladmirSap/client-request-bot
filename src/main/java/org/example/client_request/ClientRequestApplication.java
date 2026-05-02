30703190abe013fa27faf8d4925ef6ac4ba36a35
Volodymyr Sapozhnykov <vladmirsap@gmail.com>
2026-05-02 18:39:41 +0300

    Initial commit

---
+++ src/main/java/org/example/client_request/ClientRequestApplication.java
@@ -0,0 +1,13 @@
+package org.example.client_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/client_request/ClientRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.client_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
